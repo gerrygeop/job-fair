4811562b801eaa916dd3a2b860cdeb4ea44c088c
gerrygeop <gerryja28@gmail.com>
2023-06-17 18:47:02 +0800

    customize authentication

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'username' => str()->slug(fake()->firstNameMale()),
+            'username' => 'admin',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'role' => \App\Enums\UserRole::ADMIN,
         ];
