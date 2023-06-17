70d1e479e72b7010682bf9aa94c4b991ff7b1634
gerrygeop <gerryja28@gmail.com>
2023-06-17 18:28:02 +0800

    update user factory

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -7,4 +7,5 @@ enum UserRole: string
     case ADMIN = 'admin';
     case PERUSAHAAN = 'perusahaan';
     case PELAMAR = 'pelamar';
+    case VISITOR = 'visitor';
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,8 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $name = fake()->firstNameMale();
-
         return [
-            'name' => $name,
-            'username' => str()->slug($name),
+            'username' => str()->slug(fake()->firstNameMale()),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'role' => \App\Enums\UserRole::ADMIN,
         ];
