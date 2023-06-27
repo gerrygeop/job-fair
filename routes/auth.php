7ca5137138fbd01879ac23caec06be2c40e30e78
gerrygeop <gerryja28@gmail.com>
2023-06-28 01:48:58 +0800

    auth route

--- routes/auth.php
+++ routes/auth.php
@@ -2,25 +2,31 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\Auth\Perusahaan\RegisteredPerusahaanController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+    Route::get('registering', function () {
+        return view('auth.registering');
+    })->name('registering');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::prefix('register')->name('register.')->group(function () {
+        // Register Job seeker
+        Route::get('/pencari-kerja', [RegisteredUserController::class, 'create'])->name('pencari-kerja');
+        Route::post('/pencari-kerja', [RegisteredUserController::class, 'store'])->name('pencari-kerja.store');
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+        // Register Perusahaan
+        Route::get('/perusahaan', [RegisteredPerusahaanController::class, 'create'])->name('perusahaan');
+        Route::post('/perusahaan', [RegisteredPerusahaanController::class, 'store'])->name('perusahaan.store');
+    });
 
+    // Login
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 });
 
 Route::middleware('auth')->group(function () {
-
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->name('logout');
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
