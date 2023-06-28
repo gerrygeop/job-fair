e04d0c1c5d5e8b1aeacac5494add782cd1a76132
gerrygeop <gerryja28@gmail.com>
2023-06-29 06:26:33 +0800

    add slug

--- database/migrations/2023_06_23_090108_create_industris_table.php
+++ database/migrations/2023_06_23_090108_create_industris_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('industri', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
