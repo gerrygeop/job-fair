d021f4ee16a4b0749e08899d0f5b3256dca0702f
gerrygeop <gerryja28@gmail.com>
2023-07-01 02:57:17 +0800

    responsive style

--- resources/views/components/search-lowongan.blade.php
+++ resources/views/components/search-lowongan.blade.php
@@ -1,13 +1,12 @@
 <section class="bg-[url('/public/image/jigsaw.svg')] bg-blue-800">
-    <div class="max-w-7xl mx-auto px-5 py-14">
-        <div class="flex flex-col text-center w-full mb-10">
+    <div class="max-w-7xl md:max-w-3xl mx-auto px-5 py-14">
+        <div class="flex flex-col text-center w-full mb-4">
             <h1 class="text-2xl sm:text-4xl font-semibold text-white">Cari Lowongan Kerja</h1>
         </div>
 
         <form action="{{ route('lowongan-kerja') }}" method="GET">
-            <div class="flex w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
+            <div class="flex w-full sm:flex-row flex-col mx-auto sm:space-x-4 sm:space-y-0 space-y-4 items-end">
                 <div class="relative flex-grow w-full">
-                    <label for="query" class="leading-7 text-sm text-gray-50">Lowongan</label>
                     <x-text-input type="text" id="query" name="query" class="w-full" value="{{ isset($_GET['query']) ? $_GET['query'] : '' }}" placeholder="Cari Profesi, Perusahaan, Industri, Lokasi, Kota ..." />
                 </div>
 
